5a1406c2cdcfff56e2c1027e6b995dea3415c2ed
张亚飞 <672233580@qq.com>
2021-01-22 22:16:52 +0800

    Initial Commit

---
+++ Cart/CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartApp.swift
+//  Cart
+//
+//  Created by 张亚飞 on 2021/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cart
+//
+//  Created by 张亚飞 on 2021/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
